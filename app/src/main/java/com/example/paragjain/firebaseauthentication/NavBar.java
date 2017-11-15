20380beea4a19205a65125055e4b8874280584e9
xaphatro <xaphatro@gmail.com>
2017-11-15 21:54:46 +0530

    Few smaller ui changes made

--- app/src/main/java/com/example/paragjain/firebaseauthentication/NavBar.java
+++ app/src/main/java/com/example/paragjain/firebaseauthentication/NavBar.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -42,6 +43,7 @@ public class NavBar extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });*/
+
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
