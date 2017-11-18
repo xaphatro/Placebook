425fe60fc2a102e90c6b03ead939c692d58bf9f2
NikhilPr95 <nikhill.prabhu@gmail.com>
2017-11-18 18:37:31 +0530

    Checks permissions at login

--- app/src/main/java/com/example/paragjain/firebaseauthentication/LoginView.java
+++ app/src/main/java/com/example/paragjain/firebaseauthentication/LoginView.java
@@ -1,11 +1,16 @@
 package com.example.paragjain.firebaseauthentication;
 
+import android.*;
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -52,6 +57,10 @@ public class LoginView extends AppCompatActivity {
         login = (Button) findViewById(R.id.bLogin);
         signup = (TextView) findViewById(R.id.bSignUp);
 
+        if (!checkPermissions()) {
+            requestPermissions();
+        }
+
         /*
         if(session.loggedin()){
             Intent intent = new Intent(this, ListOfListsView.class);
@@ -66,6 +75,53 @@ public class LoginView extends AppCompatActivity {
         }
 
     }
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void showSnackbar(final int mainTextStringId, final int actionStringId,
+                              View.OnClickListener listener) {
+        Snackbar.make(
+                findViewById(android.R.id.content),
+                getString(mainTextStringId),
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(actionStringId), listener).show();
+    }
+
+    private void requestPermissions() {
+        final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+            showSnackbar(R.string.permission_rationale, android.R.string.ok,
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // Request permission
+                            ActivityCompat.requestPermissions(LoginView.this,
+                                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                    REQUEST_PERMISSIONS_REQUEST_CODE);
+                        }
+                    });
+        } else {
+            Log.i(TAG, "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(LoginView.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
     public static Context getInstance(){
         return loginContext;
     }
